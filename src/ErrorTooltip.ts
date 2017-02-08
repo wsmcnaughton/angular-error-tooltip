d5782b2a89ccbabe4f90a69c41ab7bb4140a8078
Will McNaughton <wsmcnaughton@gmail.com>
2017-02-08 12:05:43 -0600

    linked up FormControlName directive

--- src/ErrorTooltip.ts
+++ src/ErrorTooltip.ts
@@ -1,13 +1,12 @@
-import {
-    Directive, HostListener, ComponentRef, ViewContainerRef, Input, ComponentFactoryResolver,
-    ComponentFactory
-} from "@angular/core";
-import {ErrorTooltipContent} from "./ErrorTooltipContent";
+import { Directive, HostListener, ComponentRef, ViewContainerRef, Input, ComponentFactoryResolver, ComponentFactory, OnInit, OnDestroy } from "@angular/core";
+import { NgControl } from '@angular/forms'
+import { Subscription } from 'rxjs/Subscription';
+import { ErrorTooltipContent } from "./ErrorTooltipContent";
 
 @Directive({
-    selector: "[error-tooltip]"
+    selector: "[errorTooltip]"
 })
-export class ErrorTooltip {
+export class ErrorTooltip implements OnInit, OnDestroy {
 
     // -------------------------------------------------------------------------
     // Properties
@@ -15,21 +14,22 @@ export class ErrorTooltip {
 
     private tooltip: ComponentRef<ErrorTooltipContent>;
     private visible: boolean;
+    private statusSubscription: Subscription;
 
     // -------------------------------------------------------------------------
     // Constructor
     // -------------------------------------------------------------------------
 
-    constructor(private viewContainerRef: ViewContainerRef,
-                private resolver: ComponentFactoryResolver) {
+    constructor
+        (private viewContainerRef: ViewContainerRef,
+        private resolver: ComponentFactoryResolver,
+        private control: NgControl
+    ) {
     }
 
     // -------------------------------------------------------------------------
     // Inputs / Outputs
     // -------------------------------------------------------------------------
-
-    @Input("tooltip")
-    content: string|ErrorTooltipContent;
 
     @Input()
     tooltipDisabled: boolean;
@@ -44,44 +44,53 @@ export class ErrorTooltip {
     // Public Methods
     // -------------------------------------------------------------------------
 
-    @HostListener("focusin")
-    @HostListener("mouseenter")
-    show(): void {
-        if (this.tooltipDisabled || this.visible)
-            return;
-
-        this.visible = true;
-        if (typeof this.content === "string") {
-            const factory = this.resolver.resolveComponentFactory(ErrorTooltipContent);
-            if (!this.visible)
-                return;
-
-            this.tooltip = this.viewContainerRef.createComponent(factory);
-            this.tooltip.instance.hostElement = this.viewContainerRef.element.nativeElement;
-            this.tooltip.instance.content = this.content as string;
-            this.tooltip.instance.placement = this.tooltipPlacement;
-            this.tooltip.instance.animation = this.tooltipAnimation;
-        } else {
-            const tooltip = this.content as ErrorTooltipContent;
-            tooltip.hostElement = this.viewContainerRef.element.nativeElement;
-            tooltip.placement = this.tooltipPlacement;
-            tooltip.animation = this.tooltipAnimation;
-            tooltip.show();
-        }
+    ngOnInit() {
+        this.statusSubscription = this.control.statusChanges.subscribe(
+            status => console.log(status)  
+        );
+    }
+    ngOnDestroy() {
+        this.statusSubscription.unsubscribe();
     }
 
-    @HostListener("focusout")
-    @HostListener("mouseleave")
-    hide(): void {
-        if (!this.visible)
-            return;
+    //@HostListener("focusin")
+    //@HostListener("mouseenter")
+    //show(): void {
+    //    if (this.tooltipDisabled || this.visible)
+    //        return;
 
-        this.visible = false;
-        if (this.tooltip)
-            this.tooltip.destroy();
+    //    this.visible = true;
+    //    if (typeof this.content === "string") {
+    //        const factory = this.resolver.resolveComponentFactory(ErrorTooltipContent);
+    //        if (!this.visible)
+    //            return;
 
-        if (this.content instanceof ErrorTooltipContent)
-            (this.content as ErrorTooltipContent).hide();
-    }
+    //        this.tooltip = this.viewContainerRef.createComponent(factory);
+    //        this.tooltip.instance.hostElement = this.viewContainerRef.element.nativeElement;
+    //        this.tooltip.instance.content = this.content as string;
+    //        this.tooltip.instance.placement = this.tooltipPlacement;
+    //        this.tooltip.instance.animation = this.tooltipAnimation;
+    //    } else {
+    //        const tooltip = this.content as ErrorTooltipContent;
+    //        tooltip.hostElement = this.viewContainerRef.element.nativeElement;
+    //        tooltip.placement = this.tooltipPlacement;
+    //        tooltip.animation = this.tooltipAnimation;
+    //        tooltip.show();
+    //    }
+    //}
+
+    //@HostListener("focusout")
+    //@HostListener("mouseleave")
+    //hide(): void {
+    //    if (!this.visible)
+    //        return;
+
+    //    this.visible = false;
+    //    if (this.tooltip)
+    //        this.tooltip.destroy();
+
+    //    if (this.content instanceof ErrorTooltipContent)
+    //        (this.content as ErrorTooltipContent).hide();
+    //}
 
 }
